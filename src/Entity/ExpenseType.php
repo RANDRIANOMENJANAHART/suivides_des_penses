641353d39478caf2b32b46463fe2068970985bad
Stanis Ryan <stanisryan007@gmail.com>
2025-07-19 11:05:31 +0200

    Update ExpenseType.php

---
+++ src/Entity/ExpenseType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Expense;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ExpenseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('amount', MoneyType::class)
+            ->add('category', TextType::class)
+            ->add('date', DateType::class, [
+                'widget' => 'single_text'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Expense::class,
+        ]);
+    }
+}
